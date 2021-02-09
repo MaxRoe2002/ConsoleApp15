ee693a587958677042d1ea0b2a327a537ced4ca7
Max Roe <71944268+MaxRoe2002@users.noreply.github.com>
2021-02-09 14:54:49 +0000

    did some basic work

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,13 +1,51 @@
-﻿namespace ConsoleAppProject.App01
+﻿using System;
+
+namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// This class offers methods for converting a given distance
+    /// measured in miles to the equivalent distanced measured
+    /// in feet.
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Max Roe version 0.1
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
 
+        private double feet;
+
+        /// <summary>
+        /// Code to convert distance
+        /// </summary>
+        public void Run()
+        {
+            InputMiles();
+            CalculateFeet();
+            OutputFeet();
+        }
+
+        /// <summary>
+        /// Prompt the user to enter the distance in miles.
+        /// Input the miles as a double number.
+        /// </summary>
+        private void InputMiles()
+        {
+            Console.Write("Please enter the number of miles >");
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
+        }
+
+        private void CalculateFeet()
+        {
+
+        }
+
+        private void OutputFeet()
+        {
+
+        }
     }
 }
+
