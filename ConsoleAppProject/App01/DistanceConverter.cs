4705f5ec4bdec6cfbc8bc10d6438d6f30b906b7a
Max Roe <71944268+MaxRoe2002@users.noreply.github.com>
2021-02-11 13:11:17 +0000

    added choice system

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -3,9 +3,9 @@
 namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// This class offers methods for converting a given distance
-    /// measured in miles to the equivalent distanced measured
-    /// in feet.
+    /// This app will prompt the user to input a distance
+    /// measured in one unit (fromUnit) and it will calculate and
+    /// output the equivalent distance in another unit (toUnit).
     /// </summary>
     /// <author>
     /// Max Roe version 0.1
@@ -16,101 +16,117 @@ namespace ConsoleAppProject.App01
 
         private const double METRES_IN_MILES = 1609.34;
 
-        private double miles;
+        public const double FEET_IN_METRES = 3.28084;
 
-        private double feet;
+        public const string FEET = "feet";
+        public const string METRES = "metres";
+        public const string MILES = "miles";
 
-        private double metres;
+        private double fromDistance;
+        private double toDistance;
 
-        /// <summary>
-        /// Code to convert distance
-        /// </summary>
+        private string fromUnit;
+        private string toUnit;
 
-        public void MilesToFeet()
+        public DistanceConverter()
         {
-            OutputHeading();
-            InputMiles();
-            CalculateFeet();
-            OutputFeet();
-        }
-
-        public void FeetToMiles()
-        {
-            OutputHeading();
-            InputFeet();
-            CalculateMiles();
-            OutputMiles();
-        }
-
-        public void MilesToMetres()
-        {
-            OutputHeading();
-            InputMiles();
-            CalculateMetres();
-            OutputMetres();
+            fromUnit = MILES;
+            toUnit = FEET;
         }
 
         /// <summary>
-        /// Prompt the user to enter the distance in miles.
-        /// Input the miles as a double number.
+        /// This method will input the distance measured in miles
+        /// calculate the same distance in feet, and output the
+        /// distance in feet.
         /// </summary>
-        private void InputMiles()
+
+        public void ConvertDistance()
         {
-            Console.Write("Please enter the number of miles >");
+            fromUnit = SelectUnit("Please select the form distance unit > ");
+            toUnit = SelectUnit("Please select the to distance unit > ");
+            
+            OutputHeading($"Converting {fromUnit} to {toUnit}");
+
+            fromDistance = InputDistance($"Please enter the number of {fromUnit} > ");
+
+            //CalculateFeet();
+
+            OutputDistance();
+        }
+
+        private string SelectUnit(string prompt)
+        {
+            string choice = DisplayChoices(prompt);
+
+            return ExecuteChoice(choice);
+        }
+
+        private static string ExecuteChoice(string choice)
+        { 
+            if (choice.Equals("1"))
+            {
+                return FEET;
+            }
+            else if (choice == "2")
+            {
+                return METRES;
+            }
+            else if (choice.Equals("3"))
+            {
+                return MILES;
+            }
+
+            return null;
+        }
+
+        private static string DisplayChoices(string prompt)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"1. {FEET}");
+            Console.WriteLine($"2. {METRES}");
+            Console.WriteLine($"3. {MILES}");
+            Console.WriteLine();
+
+            Console.Write(prompt);
+            string choice = Console.ReadLine();
+            return choice;
+        }
+
+        /// <summary>
+        /// Prompt the user to enter the distance in miles,
+        /// metres or feet.
+        /// </summary>
+        private double InputDistance(string prompt)
+        {
+            Console.Write(prompt);
             string value = Console.ReadLine();
-            miles = Convert.ToDouble(value);
+            return Convert.ToDouble(value);
         }
 
-        private void InputMetres()
+        /// <summary>
+        /// Code to display the final calculations that the user has
+        /// typed in.
+        /// </summary>
+        private void OutputDistance()
         {
-            Console.Write("Please enter the number of metres >");
-            string value = Console.ReadLine();
-            metres = Convert.ToDouble(value);
+            Console.WriteLine($" {fromDistance} {fromUnit}" +
+                $" is {toDistance} {toUnit}!");
         }
 
-        private void InputFeet()
-        {
-            Console.Write("Please enter the number of feet >");
-            string value = Console.ReadLine();
-            feet = Convert.ToDouble(value);
-        }
 
-        private void CalculateFeet()
-        {
-            feet = miles * FEET_IN_MILES;
-        }
-
-        private void CalculateMiles()
-        {
-            miles = feet / FEET_IN_MILES;
-        }
-
-        private void CalculateMetres()
-        {
-            metres = miles * METRES_IN_MILES;
-        }
-
-        private void OutputFeet()
-        {
-            Console.WriteLine(miles + " miles is " + feet + " feet!");
-        }
-
-        private void OutputMiles()
-        {
-            Console.WriteLine(feet + " feet is " + miles + " miles!");
-        }
-
-        private void OutputMetres()
-        {
-            Console.WriteLine(miles + " miles is " + metres + " metres!");
-        }
-
-        private void OutputHeading()
+        /// <summary>
+        /// Code to display a heading at the top of the program,
+        /// that shows what the program is and the author.
+        /// </summary>
+        private void OutputHeading(String prompt)
         {
             Console.WriteLine("\n-----------------------------------");
-            Console.WriteLine("        Convert Miles to Feet        ");
+            Console.WriteLine("          Distance Converter         ");
             Console.WriteLine("             by Max Roe              ");
             Console.WriteLine("-----------------------------------\n");
+
+            Console.WriteLine(prompt);
+            Console.WriteLine();
         }
 
     }
