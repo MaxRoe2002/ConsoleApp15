d9689f7563f0e688b0a468d4f65f4aa6d0238073
Max Roe <maxroe666@yahoo.com>
2021-03-13 14:25:43 +0000

    did some code for student grades

---
+++ ConsoleAppProject/App03/StudentGrades.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppProject.App03
+{
+    public class StudentGrades
+    {
+        // Constants
+
+        public const int LowestMark = 0;
+        public const int LowestGradeD = 40;
+        public const int LowestGradeC = 50;
+        public const int LowestGradeB = 60;
+        public const int LowestGradeA = 70;
+        public const int HighestMark = 100;
+
+        // Properties
+
+        public string [] Students { get; set; }
+        public int[] Marks { get; set; }
+        public int[] GradeProfile { get; set; }
+        public double[] Mean { get; set; }
+        public int[] Minimum { get; set; }
+        public int[] Maximum { get; set; }
+
+        // Attributes
+        public StudentGrades()
+        {
+            Students = new string[]
+            {
+                "Luke", "Daniel", "Lewis",
+                "Aaron", "Max", "Bradley",
+                "Liam", "Thomas", "Robbie",
+                "Ben"
+            };
+            GradeProfile = new int[(int)Grades.A + 1];
+            Marks = new int[Students.Length];
+        }
+
+        /// <summary>
+        /// Input a mark between 0-100 for each 
+        /// student and store in the marks array.
+        /// </summary>
+        void InputMarks()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// List all the students and display their
+        /// name and current mark.
+        /// </summary>
+        public void OutputMarks()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Convert a student mark to a grade
+        /// from F(fail) to A (First class).
+        /// </summary>
+        public Grades ConvertToGrade(int mark)
+        {
+            if (mark >= 0 && mark < LowestGradeD)
+            {
+                return Grades.F;
+            }
+            else return Grades.D;
+        }
+
+        /// <summary>
+        /// Calculate and display the minimum, maximum
+        /// and mean mark for all the students.
+        /// </summary>
+        public void CalculateStats()
+        {
+            double total = 0;
+
+            foreach(int mark in Marks)
+            {
+                total = total + mark;
+            }
+
+            Mean = total / Marks.Length;
+        }
+
+        /// <summary>
+        /// Calculate and display the students grade 
+        /// profile.
+        /// </summary>
+        public void CalculateGradeProfile()
+        {
+            for (int i = 0; i < GradeProfile.Length; i++)
+            {
+                GradeProfile[i] = 0;
+            }
+
+            foreach (int mark in Marks)
+            {
+                Grades grade = ConvertToGrade(mark);
+                GradeProfile[(int)grade]++;
+            }
+        }
+    }
+
+}
